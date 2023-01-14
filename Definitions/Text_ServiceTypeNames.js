88de2bd88f1ea32d4c2dab2ab3d3d80f81aa0759
david <david@ellis128.co.uk>
2023-01-14 18:54:40 +0000

    Updated service names

--- Definitions/Text_ServiceTypeNames.js
+++ Definitions/Text_ServiceTypeNames.js
@@ -7,5 +7,16 @@ module.exports = [
     "CAN Service",                      // 3
     "Teaching Service",          		// 4
     "Producer Service", 				// 5
-    "Consumer Service"		            // 6
+    "Consumer Service",		            // 6
+	"Unassigned Service",				// 7
+	"Unassigned Service",				// 8
+	"Event Acknowledge Service",		// 9
+	"Boot service",						// 10
+	"Boot2 service",					// 11
+	"Fast Clock Service",				// 12
+	"DCC Cab Service",					// 13
+	"DCC Command Service".				// 14
+	"CAN Bridge Service",				// 15
+	"SLiM Service",						// 16
+	"Long Message Service"				// 17
     ];
