d1e1deb5e276204312f39b0e18f5be2ee8243353
david <david@ellis128.co.uk>
2023-01-15 07:18:42 +0000

    Added initial Service_Definitions

---
+++ Definitions/Service_Definitions.js
@@ -0,0 +1,70 @@
+'use strict';
+
+module.exports = [
+	"0":{
+		"name":"Reserved",
+		"diagnostics":{}
+	},
+	"1":{
+		"name":"Minimum Node Service",
+		"diagnostics":{}
+	},
+	"2":{
+		"name":"Node Variable Service",
+		"diagnostics":{}
+	},
+	"3":{
+		"name":"CAN Service",
+		"diagnostics":{}
+	},
+	"4":{
+		"name":"Teaching Service",
+		"diagnostics":{}
+	},
+	"5":{
+		"name":"Producer Service",
+		"diagnostics":{}
+	},
+	"6":{
+		"name":"Consumer Service",
+		"diagnostics":{}
+	},
+	// 7 - unassigned
+	// 8 - unassigned
+	"9":{
+		"name":"Event Acknowledge Service",
+		"diagnostics":{}
+	},
+	"10":{
+		"name":"Boot Service",
+		"diagnostics":{}
+	},
+	"11":{
+		"name":"Boot2 Service",
+		"diagnostics":{}
+	},
+	"12":{
+		"name":"Fast Clock Service",
+		"diagnostics":{}
+	},
+	"13":{
+		"name":"DCC Cab Service",
+		"diagnostics":{}
+	},
+	"14":{
+		"name":"DCC Command Service",
+		"diagnostics":{}
+	},
+	"15":{
+		"name":"CAN Bridge Service",
+		"diagnostics":{}
+	},
+	"16":{
+		"name":"SLiM Service",
+		"diagnostics":{}
+	},
+	"17":{
+		"name":"Long Message Service",
+		"diagnostics":{}
+	}
+];
