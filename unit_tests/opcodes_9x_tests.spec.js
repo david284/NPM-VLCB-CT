ed7e65092d9cb5c06a66d8030b67d407c8c2f10c
david <david@ellis128.co.uk>
2023-07-25 17:18:40 +0100

    started to add opcodes_9x_tests

---
+++ unit_tests/opcodes_9x_tests.spec.js
@@ -0,0 +1,83 @@
+'use strict';
+const expect = require('chai').expect;
+const winston = require('./config/winston_test.js');
+const itParam = require('mocha-param');
+const net = require('net')
+const cbusLib = require('cbusLibrary');
+const Mock_Cbus = require('./mock_CbusNetwork.js')
+const IP_Network = require('./../ip_network.js')
+const opcodes_9x = require('./../opcodes/opcodes_9x.js');
+const RetrievedValues = require('./../RetrievedValues.js');
+
+
+
+// Scope:
+// variables declared outside of the class are 'global' to this module only
+// callbacks need a bind(this) option to allow access to the class members
+// let has block scope (or global if top level)
+// var has function scope (or global if top level)
+// const has block sscope (like let), and can't be changed through reassigment or redeclared
+
+
+// Assert style
+const assert = require('chai').assert;
+
+const NET_PORT = 5569;			// 5560 + opcode catagory offset
+const NET_ADDRESS = "127.0.0.1"
+
+
+
+describe('opcodes_9x tests', function(){
+	const mock_Cbus = new Mock_Cbus.mock_CbusNetwork(NET_PORT);
+	const Network = new IP_Network.IP_Network(NET_ADDRESS, NET_PORT);
+	const tests = new opcodes_9x.opcodes_9x(Network);
+
+
+    // mns_testss have their own timeouts, so need to reflect that and add a little bit
+    // to ensure the unti tests don't timeout first
+    const test_timeout = tests.response_time + 100;
+
+	before(function() {
+		winston.info({message: ' '});
+		//                      012345678901234567890123456789987654321098765432109876543210
+		winston.info({message: '============================================================'});
+		winston.info({message: '------------------ opcodes_9x unit tests -------------------'});
+		winston.info({message: '============================================================'});
+		winston.info({message: ' '});
+
+
+	})
+    
+    beforeEach (function() {
+   		winston.info({message: ' '});   // blank line to separate tests
+		Network.messagesIn = [];
+    })
+
+	after(function(done) {
+        // bit of timing to ensure all winston messages get sent before closing tests completely
+		setTimeout(function(){
+            // timeout to allow tests to print
+            winston.info({message: ' '});   // blank line to separate tests
+            winston.info({message: 'UNIT TEST: opcodes_9x tests finished '});
+            setTimeout(function(){
+                    // timeout to allow the finish text above to print
+                     done();
+            }, 100);
+		}, 100);
+    });
+
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// 				Tests
+//
+
+    // 0x96 - NVSET
+    it("NVSET test", async function () {
+      winston.info({message: 'UNIT TEST:: BEGIN NVSET test'});
+      RetrievedValues.setNodeNumber(0);
+      winston.info({message: 'UNIT TEST: NVSET ended'});
+    })
+    
+})
+
