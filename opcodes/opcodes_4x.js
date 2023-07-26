9c3d10215c0f2fce818c1783f6c541df3aebc2f4
david <david@ellis128.co.uk>
2023-07-26 19:59:49 +0100

    Updated logging

--- opcodes/opcodes_4x.js
+++ opcodes/opcodes_4x.js
@@ -45,80 +45,74 @@ class opcodes_4x {
 
     // 0x42 SNN
 	//
-    test_SNN(RetrievedValues) {
-        return new Promise(function (resolve, reject) {
-            winston.debug({message: 'VLCB: BEGIN SNN test'});
-            this.hasTestPassed = false;
-            this.network.messagesIn = [];
-            var msgData = cbusLib.encodeSNN(RetrievedValues.getNodeNumber());
-            this.network.write(msgData);
-            setTimeout(()=>{
-                if (this.network.messagesIn.length > 0){
-                    var message = this.getMessage('NNACK');
-					if (message.nodeNumber == RetrievedValues.getNodeNumber()) {
-						winston.debug({message: 'VLCB: SNN valid'});
-						this.hasTestPassed = true;
-                    }
-                }
-
-				utils.processResult(RetrievedValues, this.hasTestPassed, 'SNN');
-				
-                resolve();
-                ;} , this.response_time
-            );
-        }.bind(this));
-    }
+  test_SNN(RetrievedValues) {
+    return new Promise(function (resolve, reject) {
+      winston.debug({message: 'VLCB: BEGIN SNN test'});
+      this.hasTestPassed = false;
+      this.network.messagesIn = [];
+      var msgData = cbusLib.encodeSNN(RetrievedValues.getNodeNumber());
+      this.network.write(msgData);
+      setTimeout(()=>{
+        if (this.network.messagesIn.length > 0){
+          var message = this.getMessage('NNACK');
+          if (message.nodeNumber == RetrievedValues.getNodeNumber()) {
+            winston.info({message: 'VLCB:      NNACK received'})
+            this.hasTestPassed = true;
+          }
+        }
+        utils.processResult(RetrievedValues, this.hasTestPassed, 'SNN');
+        resolve();
+      } , this.response_time );
+    }.bind(this));
+  }
     
 	
-    // 0x4F - NNRSM
-    test_NNRSM(RetrievedValues) {
-        return new Promise(function (resolve, reject) {
-            winston.debug({message: 'VLCB: BEGIN NNRSM test'});
-            this.hasTestPassed = false;
-            this.network.messagesIn = [];
-            var msgData = cbusLib.encodeNNRSM(RetrievedValues.getNodeNumber());
-            this.network.write(msgData);
-            setTimeout(()=>{
-				var GRSPreceived = false;
-                if (this.network.messagesIn.length > 0){
-		            this.network.messagesIn.forEach(element => {
-						var msg = cbusLib.decode(element);
-                        winston.debug({message: 'VLCB: NNRSM: msg received: ' + msg.mnemonic})
-						if (msg.mnemonic == "GRSP"){
-                            winston.debug({message: 'VLCB: NNRSM: GRSP received:'})
-							GRSPreceived = true;
-							if (msg.nodeNumber == RetrievedValues.getNodeNumber()) {
-								if (msg.requestOpCode == cbusLib.decode(msgData).opCode) {
-                                    if (msg.result == GRSP.OK) {
-    									this.hasTestPassed = true;
-                                    } else {
-                                        winston.info({message: 'VLCB: GRSP result:'
-										+ '\n  Expected ' + GRSP.OK
-										+ '\n  Actual ' + msg.result}); 
-                                    }
-								}else {
-									winston.info({message: 'VLCB: GRSP requestOpCode:'
-										+ '\n  Expected ' + cbusLib.decode(msgData).opCode
-										+ '\n  Actual ' + msg.requestOpCode}); 
-								}
-							} else {
-									winston.info({message: 'VLCB: GRSP nodeNumber:' +
-										+ '\n  Expected ' + cbusLib.decode(msgData).nodeNumber
-										+ '\n  Actual ' + msg.nodeNumber}); 
-							}
-						}
-					});
-				}
-				
-				if (!GRSPreceived) { winston.info({message: 'VLCB: NNRSM Fail: no GRSP received'}); }
-				
-				utils.processResult(RetrievedValues, this.hasTestPassed, 'NNRSM');
-				
-                resolve();
-                ;} , 1000
-            );
-        }.bind(this));
-    }
+  // 0x4F - NNRSM
+  test_NNRSM(RetrievedValues) {
+    return new Promise(function (resolve, reject) {
+      winston.debug({message: 'VLCB: BEGIN NNRSM test'});
+      this.hasTestPassed = false;
+      this.network.messagesIn = [];
+      var msgData = cbusLib.encodeNNRSM(RetrievedValues.getNodeNumber());
+      this.network.write(msgData);
+      setTimeout(()=>{
+        var GRSPreceived = false;
+        if (this.network.messagesIn.length > 0){
+          this.network.messagesIn.forEach(element => {
+            var msg = cbusLib.decode(element);
+            winston.debug({message: 'VLCB: NNRSM: msg received: ' + msg.mnemonic})
+            if (msg.mnemonic == "GRSP"){
+              winston.debug({message: 'VLCB: NNRSM: GRSP received: '})
+              GRSPreceived = true;
+              if (msg.nodeNumber == RetrievedValues.getNodeNumber()) {
+                if (msg.requestOpCode == cbusLib.decode(msgData).opCode) {
+                winston.debug({message: 'VLCB:      GRSP received: ' + msg.result})
+                  if (msg.result == GRSP.OK) {
+                    this.hasTestPassed = true;
+                  } else {
+                    winston.info({message: 'VLCB: GRSP result:'
+                      + '\n  Expected ' + GRSP.OK
+                      + '\n  Actual ' + msg.result}); 
+                  }
+                }else {
+                  winston.info({message: 'VLCB: GRSP requestOpCode:'
+                    + '\n  Expected ' + cbusLib.decode(msgData).opCode
+                    + '\n  Actual ' + msg.requestOpCode}); 
+                }
+              } else {
+                winston.info({message: 'VLCB: GRSP nodeNumber:' +
+                  + '\n  Expected ' + cbusLib.decode(msgData).nodeNumber
+                  + '\n  Actual ' + msg.nodeNumber}); 
+              }
+            }
+          });
+        }
+        if (!GRSPreceived) { winston.info({message: 'VLCB: NNRSM Fail: no GRSP received'}); }
+        utils.processResult(RetrievedValues, this.hasTestPassed, 'NNRSM');
+        resolve();
+      } , 1000 );
+    }.bind(this));
+  }
 	
 	
 
